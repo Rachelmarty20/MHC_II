bcc460db7dbf9b5859c1f64720d9b5d85f14d409
Rachelmarty20 <Rachelmarty20@gmail.com>
2017-09-12 09:49:41 -0700

    testing mhc_type gathering

--- data_gathering/mhc_types/gather.py
+++ data_gathering/mhc_types/gather.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import cPickle as pickle
 import samples
+import os
 
 
 # genes to collect
@@ -10,21 +11,21 @@ genes = ['A', 'B', 'C', 'DPA1', 'DPB1', 'DQA1', 'DQB1', 'DRA', 'DRB1', 'DRB3', '
 barcodes = samples.get_barcodes()
 
 all_patient_dictionary = {}
-for i, barcode in enumerate(barcodes[:5]):
-    print barcode
+for i, barcode in enumerate(barcodes[:2000]):
 
     patient_dictionary = {}
     directory = '/nrnb/users/ramarty/TCGA/exomes/{0}/hlaHD/sampleID/result/'.format(barcode)
 
-    for gene in genes:
-        print directory + 'sampleID_{0}.est.txt'.format(gene)
+    if os.direxists(directory):
 
-        try:
-            patient_dictionary[gene] = [':'.join(x.split(':')[:2]) for x in open(directory + 'sampleID_{0}.est.txt'.format(gene)).readlines()[2].split('\t')[:2]]
-        except:
-            patient_dictionary[gene] = ['-', '-']
+        for gene in genes:
 
-    all_patient_dictionary[barcode] = patient_dictionary
+            try:
+                patient_dictionary[gene] = [':'.join(x.split(':')[:2]) for x in open(directory + 'sampleID_{0}.est.txt'.format(gene)).readlines()[2].split('\t')[:2]]
+            except:
+                patient_dictionary[gene] = ['-', '-']
+
+        all_patient_dictionary[barcode] = patient_dictionary
 
 df = pd.DataFrame(all_patient_dictionary).transpose()
 
