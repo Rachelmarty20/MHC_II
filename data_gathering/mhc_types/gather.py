73034c69864a27f7fe47913888a970eacd0bbc08
Rachelmarty20 <Rachelmarty20@gmail.com>
2017-09-12 09:42:05 -0700

    testing mhc_type gathering

--- data_gathering/mhc_types/gather.py
+++ data_gathering/mhc_types/gather.py
@@ -10,12 +10,14 @@ genes = ['A', 'B', 'C', 'DPA1', 'DPB1', 'DQA1', 'DQB1', 'DRA', 'DRB1', 'DRB3', '
 barcodes = samples.get_barcodes()
 
 all_patient_dictionary = {}
-for barcode in barcodes[:5]:
+for i, barcode in enumerate(barcodes[:5]):
+    print barcode
 
     patient_dictionary = {}
     directory = '/nrnb/users/ramarty/TCGA/exomes/{0}/hlaHD/sampleID/result/'.format(barcode)
 
     for gene in genes:
+        print directory + 'sampleID_{0}.est.txt'.format(gene)
         patient_dictionary[gene] = [':'.join(x.split(':')[:2]) for x in open(directory + 'sampleID_{0}.est.txt'.format(gene)).readlines()[2].split('\t')[:2]]
 
     all_patient_dictionary[barcode] = patient_dictionary
