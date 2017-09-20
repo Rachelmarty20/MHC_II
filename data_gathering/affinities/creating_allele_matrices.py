d1115d2b3997c4b247f485eb873541ddb93c610d
Rachelmarty20 <Rachelmarty20@gmail.com>
2017-09-20 15:37:18 -0700

    updated creating_allele_matrices.py for all allele combinations

--- data_gathering/affinities/creating_allele_matrices.py
+++ data_gathering/affinities/creating_allele_matrices.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import sys
 
-category = sys.argv[1]
 
-alleles = [x.split('\t')[0].strip().split('*')[0] + '_' + ''.join(x.split('\t')[0].strip().split('*')[1].split(':')) for x in open('/cellar/users/ramarty/Data/hla_ii/presentation/other/alleles.txt').readlines()[1:]]
+def main(category):
 
-mutations = [x.strip() for x in open('/cellar/users/ramarty/Data/hla_ii/presentation/residues/{0}.txt'.format(category)).readlines()]
-df = pd.DataFrame({'mutation': mutations})
+    alleles = [x.strip() for x in open('/cellar/users/ramarty/Data/hla_ii/presentation/other/netMHCIIpan_alleles.txt').readlines() if x.strip()[:2] != 'H-']
 
-for i, allele in enumerate(alleles):
-    if i % 100 == 0:
-        print i
-    BR = []
-    aff = pd.read_csv('/cellar/users/ramarty/Data/hla_ii/presentation/affinities/{0}/{1}.csv'.format(category, allele), sep='\t', skiprows=1)
-    for mutation in mutations:
-        BR.append(aff[aff.ID == mutation].Rank.min())
-    df[allele] = BR
-df.to_csv('/cellar/users/ramarty/Data/hla_ii/presentation/allele_matrices/{0}.csv'.format(category))
+    mutations = [x.strip() for x in open('/cellar/users/ramarty/Data/hla_ii/presentation/residues/{0}.txt'.format(category)).readlines()]
+    df = pd.DataFrame({'mutation': mutations})
+
+    for i, allele in enumerate(alleles):
+        BR = []
+        aff = pd.read_csv('/cellar/users/ramarty/Data/hla_ii/presentation/affinities/{0}/{1}.csv'.format(category, allele), sep='\t', skiprows=1)
+        for mutation in mutations:
+            BR.append(aff[aff.ID == mutation].Rank.min())
+        df[allele] = BR
+    df.to_csv('/cellar/users/ramarty/Data/hla_ii/presentation/allele_matrices/{0}.csv'.format(category))
+
+
+
+###########################################  Main Method  #####################################
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print 'Wrong number of arguments.'
+        sys.exit()
+    main(sys.argv[1])
+    sys.exit()
