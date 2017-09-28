9c0379bbe29c3f7a5466f75dbc54dfb5b2a88863
Rachelmarty20 <Rachelmarty20@gmail.com>
2017-09-28 08:17:45 -0700

    controlled for non-typed

--- data_gathering/mhc_types/gather.py
+++ data_gathering/mhc_types/gather.py
@@ -7,38 +7,41 @@ barcodes = samples.get_barcodes()
 
 all_patient_dictionary = {}
 for i, barcode in enumerate(barcodes):
+    print barcode
 
     patient_dictionary = {}
     directory = '/nrnb/users/ramarty/TCGA/exomes/{0}/hlaHD/sampleID/result/'.format(barcode)
 
-    try:
-        lines = [x.split('\t') for x in open('{0}sampleID_final.result.txt'.format(directory)).readlines()[:8]]
 
-        patient_dictionary = {}
-        for line in lines:
-            if line[0] in ['DRB1']:
-                if line[2].strip() == '-':
-                    x = line[1]
-                    alleles = [line[0]+'_'+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1]]*2
-                else:
-                    alleles = [line[0]+'_'+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1] for x in line[1:3]]
-                patient_dictionary[line[0]+'_allele1'] = alleles[0]
-                patient_dictionary[line[0]+'_allele2'] = alleles[1]
-            # combinations
-            elif line[0] in ['DQA1', 'DQB1', 'DPA1', 'DPB1']:
-                if line[2].strip() == '-':
-                    x = line[1]
-                    alleles = [line[0]+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1]]*2
-                else:
-                    alleles = [line[0]+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1] for x in line[1:3]]
-                patient_dictionary[line[0]+'_allele1'] = alleles[0]
-                patient_dictionary[line[0]+'_allele2'] = alleles[1]
-                all_patient_dictionary[barcode] = patient_dictionary
+    lines = [x.split('\t') for x in open('{0}sampleID_final.result.txt'.format(directory)).readlines()[:8]]
+
+    patient_dictionary = {}
+    for line in lines:
+        if line[0] in ['DRB1']:
+            if line[1] == 'Not typed':
+                alleles = ['-', '-']
+            elif line[2].strip() == '-':
+                x = line[1]
+                alleles = [line[0]+'_'+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1]]*2
             else:
-                None
+                alleles = [line[0]+'_'+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1] for x in line[1:3]]
+            patient_dictionary[line[0]+'_allele1'] = alleles[0]
+            patient_dictionary[line[0]+'_allele2'] = alleles[1]
+        # combinations
+        elif line[0] in ['DQA1', 'DQB1', 'DPA1', 'DPB1']:
+            if line[1] == 'Not typed':
+                alleles = ['-', '-']
+            elif line[2].strip() == '-':
+                x = line[1]
+                alleles = [line[0]+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1]]*2
+            else:
+                alleles = [line[0]+x.split('*')[1].split(':')[0]+x.split('*')[1].split(':')[1] for x in line[1:3]]
+            patient_dictionary[line[0]+'_allele1'] = alleles[0]
+            patient_dictionary[line[0]+'_allele2'] = alleles[1]
+            all_patient_dictionary[barcode] = patient_dictionary
+        else:
+            None
 
-    except:
-        print 'skip' + barcode
 
 df = pd.DataFrame(all_patient_dictionary).transpose()
 
@@ -55,20 +58,24 @@ for patient in patients:
     try:
         # DR
         alleles = list(patient_types_df.ix[patient][['DRB1_allele1', 'DRB1_allele2']])
+        if '-' in alleles:
+            continue
 
         # DP
         DA = list(patient_types_df.ix[patient][['DPA1_allele1', 'DPA1_allele2']])
         DB = list(patient_types_df.ix[patient][['DPB1_allele1', 'DPB1_allele2']])
-        for a in DA:
-            for b in DB:
-                alleles.append('HLA-{0}-{1}'.format(a.strip(), b.strip()))
+        if ('-' not in DA) & ('-' not in DB):
+            for a in DA:
+                for b in DB:
+                    alleles.append('HLA-{0}-{1}'.format(a.strip(), b.strip()))
 
         # DQ
         DA = list(patient_types_df.ix[patient][['DQA1_allele1', 'DQA1_allele2']])
         DB = list(patient_types_df.ix[patient][['DQB1_allele1', 'DQB1_allele2']])
-        for a in DA:
-            for b in DB:
-                alleles.append('HLA-{0}-{1}'.format(a.strip(), b.strip()))
+        if ('-' not in DA) & ('-' not in DB):
+            for a in DA:
+                for b in DB:
+                    alleles.append('HLA-{0}-{1}'.format(a.strip(), b.strip()))
         dictionary[patient] = alleles
     except:
         # weeds out the patients with the failed typing
